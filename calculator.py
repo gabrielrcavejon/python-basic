6796d3bed51aef9f2d38c060973d3dd88969b54e
gabrielrcavejon <gabriel.cavejon@hotmail.com>
2024-05-16 22:21:53 -0300

    commit

--- calculator.py
+++ calculator.py
@@ -1,9 +1,13 @@
+'''
+Me chamo Gabriel Rodrigo Cavejon, este arquivo calculator.py, na data 16/05/2024 fiz esse projeto
+'''
+
 print("Escolha uma operação.")
 print()
-print("A - Adição")
-print("S - Subtração")
-print("M - Multiplicação")
-print("D - Divisão")
+print("1 - Adição")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
 print("X - Sair")
 print()
 
@@ -12,41 +16,76 @@ operacao = ""
 while operacao != "X" or operacao != "x":
 
   operacao = input()
+  resultado = 0
 
-  match operacao:
-    case "A":
-      n1 = int(input("Digite o primeiro numero: "))
-      n2 = int(input("Digite o segundo numero: "))
-      
-      print(f"Resultado de {n1} + {n2} = {n1 + n2}")
+  '''
+    MATCH CASE principal, dentro de um while, ate quando nao for igual a X ou x que é sair
+  '''
+
+  match operacao: # AQUI vai ser escolhido o que vai ser feito com base no que o usuario digitou
+    case "1": 
+      while True:
+        num = input("Digite um numero ou aperte P para calcular: ")
+
+        if num == "P":
+          print(f"Resultado é {resultado}")
+          break
+
+        resultado = resultado + float(num)
       print()
-    case "S":
-      n1 = int(input("Digite o primeiro numero: "))
-      n2 = int(input("Digite o segundo numero: "))
-      
-      print(f"Resultado de {n1} - {n2} = {n1 - n2}")
+    case "2":
+      while True:
+        num = input("Digite um numero ou aperte P para calcular: ")
+
+        if num == "P":
+          print(f"Resultado é {resultado}")
+          break
+        
+        if resultado == 0:
+          resultado = float(num)
+          print(resultado)
+        else:
+          resultado = resultado - float(num)
+          print(resultado)
+
       print()
-    case "M":
-      n1 = int(input("Digite o primeiro numero: "))
-      n2 = int(input("Digite o segundo numero: "))
-      
-      print(f"Resultado de {n1} * {n2} = {n1 * n2}")
+    case "3":
+      while True:
+        num = input("Digite um numero ou aperte P para calcular: ")
+
+        if num == "P":
+          print(f"Resultado é {resultado}")
+          break
+        
+        if resultado == 0:
+          resultado = float(num)
+        else:
+          resultado = resultado * float(num)
+
       print()
-    case "D":
-      n1 = int(input("Digite o primeiro numero: "))
-      n2 = int(input("Digite o segundo numero: "))
-      
-      print(f"Resultado de {n1} / {n2} = {n1 / n2}")
+    case "4":
+      while True:
+        num = input("Digite um numero ou aperte P para calcular: ")
+
+        if num == "P":
+          print(f"Resultado é {resultado}")
+          break
+        
+        if resultado == 0:
+          resultado = float(num)
+        else:
+          resultado = resultado / float(num)
+
       print()
     case "X":
       break
-    case _:
+    case _: # AQUI quando foi algo que nao é 1, 2, 3, 4 ou X
       print("Digite um número valido!")
       
   operacao = ""
+  resultado = 0
 
   print("Escolha outra operação.")
 
-  #limpar terminal
 
 print("Obrigado por usar Calculator!")
